9741f0c12a48300e3b04a9423f8938346d139dc4
Niels Abspoel <aboe@spectre.wraith.lan>
2019-01-27 15:06:09 +0100

    fix states

---
+++ tests/integration/test_salt.py
@@ -0,0 +1,13 @@
+def test_memcached_is_installed(host):
+    memcached = host.package("memcached")
+    assert memcached.is_installed
+
+def test_memcached_running_and_enabled(host):
+    memcached = host.service("memcached")
+    assert memcached.is_running
+    assert memcached.is_enabled
+
+def test_memached_is_listening(host):
+    memcached = host.socket("tcp://127.0.0.1:11211")
+    assert memcached.is_listening
+

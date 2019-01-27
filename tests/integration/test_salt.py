ed8afd6e0d79dbec0aa6abcd1ecca2c6366f8607
Niels Abspoel <aboe@spectre.wraith.lan>
2019-01-27 15:47:08 +0100

    simplify

--- tests/integration/test_salt.py
+++ tests/integration/test_salt.py
@@ -5,9 +5,3 @@ def test_memcached_is_installed(host):
 def test_memcached_running_and_enabled(host):
     memcached = host.service("memcached")
     assert memcached.is_running
-    assert memcached.is_enabled
-
-def test_memached_is_listening(host):
-    memcached = host.socket("tcp://11211")
-    assert memcached.is_listening
-
